f0a2553bac5b65698ad2a7a48342033e8387e3ef
legendateam <undertaker176@gmail.com>
2022-06-29 19:58:13 +0300

    added data in config file and implements data of config file

--- Server/src/configs/main.config.ts
+++ Server/src/configs/main.config.ts
@@ -4,4 +4,23 @@ dotenv.config();
 
 export const mainConfig = {
     PORT: process.env.PORT || 2000,
+    NAME_DATABASE: process.env.NAME_DATABASE || 'root',
+    USER_NAME_DATABASE: process.env.USER_NAME_DATABASE || 'root',
+    HOST_DATABASE: process.env.HOST_DATABASE || 'localhost',
+    PORT_DATABASE: process.env.PORT_DATABASE || 5432,
+    PASSWORD_DATABASE: process.env.PORT_DATABASE || 'root',
+    DOMAIN_NAME: process.env.DOMAIN_NAME || 'https://my-library.ua/',
+    USER_PASSWORD_SALT_ROUNDS: process.env.USER_SALT_ROUNDS || 7,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY || 'secret_access',
+    SECRET_REFRESH_KEY: process.env.SECRET_REFRESH_KEY || 'secret_refresh',
+    SECRET_FORGOT_PASSWORD_KEY: process.env.SECRET_FORGOT_PASSWORD_KEY || 'secret_forgot',
+    EXPIRES_IN_ACCESS: process.env.EXPIRES_IN_ACCESS || '1h',
+    EXPIRES_IN_REFRESH: process.env.EXPIRES_IN_REFRESH || '8h',
+    EXPIRES_IN_FORGOT_PASSWORD: process.env.EXPIRES_IN_FORGOT_PASSWORD || '30m',
+    ROOT_EMAIL: process.env.ROOT_EMAIL,
+    ROOT_EMAIL_PASSWORD: process.env.ROOT_EMAIL_PASSWORD,
+    S3_NAME: process.env.S3_NAME,
+    S3_REGION: process.env.S3_REGION,
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_KEY: process.env.S3_SECRET_KEY,
 };
--- Server/src/ormconfig.ts
+++ Server/src/ormconfig.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
+import { mainConfig } from './configs';
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'root',
-    database: 'postgres',
+    host: mainConfig.HOST_DATABASE,
+    port: Number(mainConfig.PORT_DATABASE),
+    username: mainConfig.USER_NAME_DATABASE,
+    password: mainConfig.PASSWORD_DATABASE,
+    database: mainConfig.NAME_DATABASE,
     synchronize: false,
     logging: false,
     migrations: [
