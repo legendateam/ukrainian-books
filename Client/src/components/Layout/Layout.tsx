25a0ddeb85ab488e4130018a68e299a2008da94c
legendateam <undertaker176@gmail.com>
2022-07-17 14:02:52 +0300

    add config eslint and ts

---
+++ Client/src/components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import css from './layout.module.css';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+export const Layout = () => (
+    <div>
+        <Header />
+        <div className={css.content}>
+            <Outlet />
+        </div>
+        <Footer />
+    </div>
+);
